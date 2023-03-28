e4f4a825e995ba297ce90ad9686deb18e9bd7926
RdotZz <3210817013@qq.com>
2023-03-28 16:06:43 +0800

    the new 函数

--- ch2/fnew/functionaboutnew.go
+++ ch2/fnew/functionaboutnew.go
@@ -7,8 +7,16 @@ func main() {
 	初始化type类型的零值，然后返回变量地址，
 	返回类型为*type*/
 	//new(type)*type
-	p := new(int)  //p为*int类型，指向匿名的int变量
-	fmt.Println(p) //1
+	p := new(int)   //p为*int类型，指向匿名的int变量
+	fmt.Println(*p) //1
 	*p = 2
-	fmt.Println(p) //2
+	fmt.Println(*p) //2
+	compare()
+}
+
+func compare() {
+	//每次调用new函数都是返回一个新的变量的地址
+	a := new(int)
+	b := new(int)
+	fmt.Println(a == b) //"false"
 }
