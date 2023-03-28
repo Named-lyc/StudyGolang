be3a5a20e6586022ee08254fa6ac49cb66832253
RdotZz <3210817013@qq.com>
2023-03-28 14:56:46 +0800

    server2

---
+++ ch1/server2/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+// 处理程序显示所请求url路径
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Paht = %q\n", r.URL.Path)
+}
+
+//处理程序显示请求次数
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "count %d\n", count)
+	mu.Unlock()
+}
