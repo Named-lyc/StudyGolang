a6f39b3d8ae6fcb9be4501c87cc74d2ca1dc1068
RdotZz <3210817013@qq.com>
2023-04-03 19:06:02 +0800

    make(type) new(type)*type

--- ch2/fnew/functionaboutnew.go
+++ ch2/fnew/functionaboutnew.go
@@ -5,13 +5,14 @@ import "fmt"
 func main() {
 	/*new(type)会创建一个type类型的匿名变量，
 	初始化type类型的零值，然后返回变量地址，
-	返回类型为*type*/
+	返回类型为*type   */
 	//new(type)*type
 	p := new(int)   //p为*int类型，指向匿名的int变量
 	fmt.Println(*p) //1
 	*p = 2
 	fmt.Println(*p) //2
 	compare()
+
 }
 
 func compare() {
