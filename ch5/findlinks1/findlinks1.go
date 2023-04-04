046a8bb95bdc136a991abe9b0249ea856fe3339f
RdotZz <3210817013@qq.com>
2023-04-04 14:12:48 +0800

    递归调用

---
+++ ch5/findlinks1/findlinks1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1:%v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, attribute := range n.Attr {
+			if attribute.Key == "href" {
+				links = append(links, attribute.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
