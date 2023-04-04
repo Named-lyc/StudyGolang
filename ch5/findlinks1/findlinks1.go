1824ae850ccee6dc48eb472831ba23ee9d9b4cfe
RdotZz <3210817013@qq.com>
2023-04-04 14:33:49 +0800

    递归调用

--- ch5/findlinks1/findlinks1.go
+++ ch5/findlinks1/findlinks1.go
@@ -30,3 +30,25 @@ func visit(links []string, n *html.Node) []string {
 	}
 	return links
 }
+
+// 递归版本
+func visitRecursive(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	return visitRecursion(links, n.FirstChild)
+}
+
+func visitRecursion(links []string, child *html.Node) []string {
+	//结束条件
+	if child == nil {
+		return links
+	}
+	links = visitRecursive(links, child)
+	child = child.NextSibling
+	return visitRecursion(links, child)
+}
